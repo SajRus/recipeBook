fdb82d080ec23c6d924587a26eeb1361b39875f1
sajRus <sajmir.rusi@gmail.com>
2017-06-27 14:07:00 +0200

    Update ricette.component.ts
    fix merge conflicts

--- src/app/ricette/ricette.component.ts
+++ src/app/ricette/ricette.component.ts
@@ -2,38 +2,25 @@
 
 
 import { Component, OnInit } from '@angular/core';
-import { Ricette } from './ricette.model';
-<<<<<<< HEAD
-import { RicetteService } from "app/ricette/recette.service";
-=======
+// import { Ricette } from './ricette.model';
 import { RicetteService } from './ricette.service';
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
 
 @Component({
   selector: 'app-ricette',
   templateUrl: './ricette.component.html',
-  styleUrls: ['./ricette.component.css'],
-  providers: [RicetteService]
+  styleUrls: ['./ricette.component.css']
 })
 export class RicetteComponent implements OnInit {
-<<<<<<< HEAD
- ricettaEl: Ricette;
-=======
- ricettaSelezionata: Ricette;
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
-  constructor(private ricetteService: RicetteService) { }
+//  ricettaSelezionata: Ricette;
+  constructor(/*private ricetteService: RicetteService*/) { }
 
   ngOnInit() {
-    this.ricetteService.ricettaSelezionata.subscribe(
-      (ricetta: Ricette) => {
-<<<<<<< HEAD
-        this.ricettaEl =  ricetta;
-=======
-        this.ricettaSelezionata = ricetta;
-        console.log(ricetta);
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
-      }
-    )
+    // this.ricetteService.ricettaSelezionata.subscribe(
+    //   (ricetta: Ricette) => {
+    //     this.ricettaSelezionata = ricetta;
+    //     console.log(ricetta);
+    //   }
+    // )
   }
 
 }
