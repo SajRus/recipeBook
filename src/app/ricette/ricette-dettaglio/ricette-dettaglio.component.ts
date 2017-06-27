5daffaeadaad462281418bb06b10af4dd2ceb169
sajRus <sajmir.rusi@gmail.com>
2017-06-27 14:08:05 +0200

    Update ricette-dettaglio.component.ts
    fix merge conflicts

--- src/app/ricette/ricette-dettaglio/ricette-dettaglio.component.ts
+++ src/app/ricette/ricette-dettaglio/ricette-dettaglio.component.ts
@@ -1,33 +1,23 @@
-<<<<<<< HEAD
-
-
 import { Component, OnInit } from '@angular/core';
-import { Ricette } from '../ricette.model'
-import { RicetteService } from "app/ricette/recette.service";
-import { Params, ActivatedRoute } from "@angular/router";
-=======
-import { Component, Input } from '@angular/core';
 import { Ricette } from '../ricette.model';
 import { ListaSpesaService } from '../../lista-spesa/lista-spesa.service';
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
+import { RicetteService } from '../ricette.service';
+import { ActivatedRoute, Params, Routes, Router } from '@angular/router';
 
 @Component({
     selector: 'app-ricette-dettaglio',
     templateUrl: './ricette-dettaglio.component.html'
 })
 
-<<<<<<< HEAD
 export class RicetteDettaglio implements OnInit{
     ricetta: Ricette;
     id: number;
 
-
-    constructor(private ricetteService: RicetteService,
-                private route: ActivatedRoute) { }
-
-
-    addIngredienti() {
-        this.ricetteService.addIngredienti(this.ricetta.ingredienti);
+    constructor( private ricettaService: RicetteService,
+                private route: ActivatedRoute,
+                private router: Router){}
+    addIngredientiAllaListaSpesa(){
+        this.ricettaService.addIngredientiListaSpesa(this.ricetta.ingredienti);
     }
 
     ngOnInit(){
@@ -37,16 +27,12 @@ export class RicetteDettaglio implements OnInit{
         this.route.params.subscribe(
             (params: Params) => {
                 this.id = params['id'];
-                this.ricetta = this.ricetteService.getRicetta(this.id);
+                this.ricetta = this.ricettaService.getRicetta(this.id);
             }
         )
-=======
-export class RicetteDettaglio {
-    @Input() ricetta: Ricette;
-
-    constructor(private lsService: ListaSpesaService){}
-    addIngredientiAllaListaSpesa(){
-        this.lsService.addIngredienti(this.ricetta.ingredienti);
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
+    }
+    onDeleteRicetta(){
+        this.ricettaService.deleteRicetta(this.id);
+        this.router.navigate(['/ricette']);
     }
 }
