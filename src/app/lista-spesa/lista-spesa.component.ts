ab86317d890690e9ed74322233610a1266b049a3
sajRus <sajmir.rusi@gmail.com>
2017-06-27 14:09:43 +0200

    Update lista-spesa.component.ts
    fix merge conflicts

--- src/app/lista-spesa/lista-spesa.component.ts
+++ src/app/lista-spesa/lista-spesa.component.ts
@@ -1,44 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredienti } from '../shared/app.ingredienti.model';
-<<<<<<< HEAD
-import { ListaSpesaService } from "app/lista-spesa/lista-spesa.service";
-=======
 import { ListaSpesaService } from './lista-spesa.service';
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
+import { Subscription } from "rxjs/Subscription";
 @Component({
   selector: 'app-lista-spesa',
   templateUrl: './lista-spesa.component.html',
   styleUrls: ['./lista-spesa.component.css']
 })
-export class ListaSpesaComponent implements OnInit {
-<<<<<<< HEAD
-  ingedienti: Ingredienti[] = [];
-
-  constructor(private lsService: ListaSpesaService) { }
-
-  ngOnInit() {
-      this.ingedienti = this.lsService.getIngredienti();
-
-      this.lsService.ingredientiAggiornati.subscribe(
-        (ingredienti: Ingredienti[]) => {
-          this.ingedienti = ingredienti;
-        }
-      )
-  }
-
-  onIngredienteAggiunto(value: Ingredienti){
-    this.ingedienti.push(value);
-=======
+export class ListaSpesaComponent implements OnInit, OnDestroy {
   ingedienti: Ingredienti[];
   constructor(private listaSpesaService: ListaSpesaService) { }
+  private subscription: Subscription;
 
   ngOnInit() {
     this.ingedienti = this.listaSpesaService.getIngredienti();
-    this.listaSpesaService.ingredientiAggiornati.subscribe(
+    this.subscription = this.listaSpesaService.ingredientiAggiornati.subscribe(
       (ingredienti: Ingredienti[]) => {
         this.ingedienti = ingredienti;
       }
     )
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
+  }
+
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
+  }
+
+  onItemEdit(i: number) {
+      this.listaSpesaService.ingredienteEditing.next(i);
   }
 }
