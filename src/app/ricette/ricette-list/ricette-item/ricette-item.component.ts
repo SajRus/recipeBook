d8feb8ecdca4b8a4f04a61be1c08b55b493449cd
sajRus <sajmir.rusi@gmail.com>
2017-06-27 14:05:48 +0200

    Update ricette-item.component.ts
    fix merger with develop bugs

--- src/app/ricette/ricette-list/ricette-item/ricette-item.component.ts
+++ src/app/ricette/ricette-list/ricette-item/ricette-item.component.ts
@@ -2,12 +2,7 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 import { Ricette } from '../../ricette.model';
-<<<<<<< HEAD
-import { RicetteService } from "app/ricette/recette.service";
-=======
 
-import { RicetteService } from '../../ricette.service';
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
 @Component({
   selector: 'app-ricette-item',
   templateUrl: './ricette-item.component.html',
@@ -15,21 +10,10 @@ import { RicetteService } from '../../ricette.service';
 })
 export class RicetteItemComponent implements OnInit {
   @Input() ricetta: Ricette;
-<<<<<<< HEAD
   @Input() index: number;
-=======
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
 
-  constructor(private ricetteService: RicetteService) { }
+  constructor() { }
 
   ngOnInit() {
   }
-
-<<<<<<< HEAD
-=======
-  onSelected(){
-      this.ricetteService.ricettaSelezionata.emit(this.ricetta);
-  }
-
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
 }
