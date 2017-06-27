081a17918177c1f425cc9df516a53dd6ed7cef11
sajRus <sajmir.rusi@gmail.com>
2017-06-27 14:06:32 +0200

    Update ricette.model.ts
    fix merge conflicts

--- src/app/ricette/ricette.model.ts
+++ src/app/ricette/ricette.model.ts
@@ -1,16 +1,5 @@
 import { Ingredienti } from '../shared/app.ingredienti.model';
 
-<<<<<<< HEAD
-import { Ingredienti } from "app/shared/app.ingredienti.model";
-
-export class Ricette {
-    constructor(
-        public name: string,
-        public desc: string,
-        public imgPath: string,
-        public ingredienti: Ingredienti[]
-    ) { }
-=======
 export class Ricette{
     // public name: string;
     // public desc: string;
@@ -22,5 +11,4 @@ export class Ricette{
     //     this.imgPath = imgPath;
     // }
     
->>>>>>> a9219a16ca6e889b88ca4abdd58cddb7ed19e20c
 }
